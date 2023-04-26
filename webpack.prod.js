fb3d9a37cee815cdb3dfde19f042512cc1c9ed33
Mac Signer <macsigner@gmx.ch>
2023-04-26 08:48:46 +0200

    feat: add cleaning of dist folder

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,8 +1,12 @@
 import {merge} from 'webpack-merge';
 import settings from './webpack.config.js';
+import {CleanWebpackPlugin} from 'clean-webpack-plugin';
 
 const prodSettings = merge(settings, {
     mode: 'production',
+    plugins: [
+        new CleanWebpackPlugin(),
+    ]
 })
 
 export default prodSettings;
